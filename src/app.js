d4470a4ad01e9a89ec43c3e83beddf727c7e3558
Stefano Imperial Salvador <105392252+is-stefano@users.noreply.github.com>
2024-06-25 16:11:11 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexpalos = Math.floor(Math.random() * palos.length);
+  let indexnumbers = Math.floor(Math.random() * numbers.length);
+
+  let topElement = document.querySelector("#top");
+  let numbersElement = document.querySelector("#numbers");
+  let bottomElement = document.querySelector("#bottom");
+
+  let palo = palos[indexpalos];
+  let number = numbers[indexnumbers];
+
+  topElement.innerHTML = palo;
+  numbersElement.innerHTML = number;
+  bottomElement.innerHTML = palo;
+
+  if (palo === "♦" || palo === "♥") {
+    topElement.classList.add("red");
+    bottomElement.classList.add("red");
+  } else {
+    topElement.classList.remove("red");
+    bottomElement.classList.remove("red");
+  }
 };
